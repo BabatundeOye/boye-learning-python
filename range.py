dbb03037752c22be4f50eb728d2b3faeb3879eab
BabatundeOye <teamsoldd@gmail.com>
2022-09-05 19:38:32 +0000

    working

---
+++ range.py
@@ -0,0 +1,15 @@
+
+#for i in range(5):
+    #print(i)
+    
+# range(start, stop)
+#for i in range(3,8):
+    #print(i)
+    
+# range (start, stop, step)    
+#for i in range(3, 12, 2):
+    #print(i)
+    
+for num in range(1, 10, 2):
+    if num % 3:
+        print(num)
